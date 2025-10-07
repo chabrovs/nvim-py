4427785824d28befbab0fc56c3056e917b9e5a75
Segei Chabrov <sergei200110@gmail.com>
2025-10-07 11:55:42 +0300

    feat (keymaps): added support to exit the I mode from vertual terminal.

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "UndoTree" })
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
