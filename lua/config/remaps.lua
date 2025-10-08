66c9c39d8a4d4a8091edd614f4da728a1e63f428
Segei Chabrov <sergei200110@gmail.com>
2025-10-08 13:24:28 +0300

    feat (keymaps): changes scroll size.

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "UndoTree" })
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
-vim.keymap.set("n", "<PageUp>", "15<C-Y>", { noremap = true, silent = true })
-vim.keymap.set("n", "<PageDown>", "15<C-E>", { noremap = true, silent = true })
+vim.keymap.set("n", "<PageUp>", "2<C-Y>", { noremap = true, silent = true })
+vim.keymap.set("n", "<PageDown>", "2<C-E>", { noremap = true, silent = true })
